1f5056bee024d388c8cf98d062d5bfaa521bbc0f
Frank Mittelbach <frank.mittelbach@latex-project.org>
2023-05-10 11:56:10 +0200

    fix for #10

--- build.lua
+++ build.lua
@@ -24,7 +24,7 @@ checkruns     = 2
 
 uploadconfig = {
  pkg = "unicodefonttable",
- version = "v1.0g 2022-11-12",
+ version = "v1.0h 2023-05-10",
  author = "Frank Mittelbach",
  license = "lppl1.3c",
  summary = "A Unicode font table generator",
